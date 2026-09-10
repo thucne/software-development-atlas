abd98a1e04f5a13bae0d21a5e887303dc9d583f8
Tran Trong Thuc <55175628+thucne@users.noreply.github.com>
2026-09-10 22:26:57 +0700

    docs: update September release lesson count

--- components/atlas/release-banner.tsx
+++ components/atlas/release-banner.tsx
@@ -17,8 +17,8 @@ export function AtlasReleaseBanner({ locale = 'en' }: { locale?: string }) {
         </span>
         <span>
           {isVi
-            ? '20 bài học kiến trúc hệ thống mới vừa ra mắt!'
-            : '20 new system architecture lessons added!'}
+            ? '21 bài học kiến trúc hệ thống mới vừa ra mắt!'
+            : '21 new system architecture lessons added!'}
         </span>
         <Link
           href={changelogHref}
