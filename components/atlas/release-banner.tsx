2cde82a8916a672d6c59a6130f9da4432c8ec65c
Tran Trong Thuc <55175628+thucne@users.noreply.github.com>
2026-09-11 10:04:16 +0700

    docs: bump release banner to 28 lessons

--- components/atlas/release-banner.tsx
+++ components/atlas/release-banner.tsx
@@ -17,8 +17,8 @@ export function AtlasReleaseBanner({ locale = 'en' }: { locale?: string }) {
         </span>
         <span>
           {isVi
-            ? '27 bài học kiến trúc hệ thống mới vừa ra mắt!'
-            : '27 new system architecture lessons added!'}
+            ? '28 bài học kiến trúc hệ thống mới vừa ra mắt!'
+            : '28 new system architecture lessons added!'}
         </span>
         <Link
           href={changelogHref}
